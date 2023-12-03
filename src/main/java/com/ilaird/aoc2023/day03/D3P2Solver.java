f25e47f2a50e53a1864c4e0104842be7ca067a21
Ian Laird <irlaird@gmail.com>
2023-12-03 11:38:53 -0700

    Day 3 part 2 fixed.

--- src/main/java/com/ilaird/aoc2023/day03/D3P2Solver.java
+++ src/main/java/com/ilaird/aoc2023/day03/D3P2Solver.java
@@ -2,7 +2,6 @@ package com.ilaird.aoc2023.day03;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -21,38 +20,38 @@ class D3P2Solver implements Solver {
 
     @Override
     public int solve() throws SolverError {
+
         var ret = 0;
-        var astriscCount = 0;
-        var gearCount = 0;
         var grid = new Grid();
         var seen = new HashSet<>();
+
         input.forEach(r -> grid.addGridRow(r));
+
         for (Point sp : grid.getSymbolOffsets('*')) {
-            astriscCount++;
-            System.out.println("SYMBOL: " + sp);
+
             var gearVals = new ArrayList<Integer>();
+
             for (Point tp : sp.getAdjacentPoints()) {
-                var ident = Objects.hash(sp, grid.getIdentityAt(tp));
+
+                var ident = mkIdentity(sp, grid.getIdentityAt(tp));
                 if (!seen.contains(ident)) {
                     seen.add(ident);
                     var gv = grid.getValueAt(tp);
-                    if (gv > 0) {
+                    if (gv > 0)
                         gearVals.add(gv);
-                        System.out.println("        " + tp + " -> " + gv + " (" + gearVals + ")");
-                    }
                 }
             }
 
-            if (gearVals.size() == 2) {
-                var product = gearVals.stream().reduce(1, (a, b) -> a * b);
-                ret += product;
-                gearCount++;
-                System.out.println("        " + product + " -> " + ret);
-            }
+            if (gearVals.size() == 2)
+                ret += gearVals.stream().reduce(1, (a, b) -> a * b);
         }
-        System.out.println("Astriscs: " + astriscCount);
-        System.out.println("Gears: " + gearCount);
-        // to low : 86783783
+
         return ret;
+    }
+
+    Point mkIdentity(Point a, Point b) {
+        var x = (a.x * 61) + (b.x);
+        var y = (a.y * 61) + (b.y);
+        return new Point(x, y);
     }
 }
